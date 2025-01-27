85d925fe512fbba4d6e377c2563250d8660d25cd
Caique Castro Rodrigues <fh203@t-systems.com.br>
2025-01-26 23:47:56 -0300

    [FIX] - Ajuste na program para mostrar o swagger no ambiente da azure

--- GerenciadorCampeonatos/GerenciadorCampeonatos.WebApi/Program.cs
+++ GerenciadorCampeonatos/GerenciadorCampeonatos.WebApi/Program.cs
@@ -8,10 +8,16 @@ using System.Reflection;
 
 var builder = WebApplication.CreateBuilder(args);
 
-// Add services to the container.
+var connectionString = builder.Configuration.GetConnectionString("DefaultConnection");
+builder.Services.AddDbContextFactory<CampeonatosDbContext>(options =>
+    options.UseSqlServer(connectionString));
+
+builder.Services.AddAuthorization();
+
+builder.Services.AddIdentityApiEndpoints<IdentityUser>()
+    .AddEntityFrameworkStores<CampeonatosDbContext>();
 
 builder.Services.AddControllers();
-// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen(options =>
 {
@@ -51,27 +57,14 @@ builder.Services.AddScoped<ITeamService, TeamService>();
 builder.Services.AddScoped<IPlayerService, PlayerService>();
 builder.Services.AddScoped<IMatchService, MatchService>();
 
-var connectionString = builder.Configuration.GetConnectionString("DefaultConnection");
-builder.Services.AddDbContextFactory<CampeonatosDbContext>(options =>
-    options.UseSqlServer(connectionString));
-
-builder.Services.AddAuthorization();
-
-builder.Services.AddIdentityApiEndpoints<IdentityUser>()
-    .AddEntityFrameworkStores<CampeonatosDbContext>();
-
 var app = builder.Build();
 
 app.MapIdentityApi<IdentityUser>();
 
-if (app.Environment.IsDevelopment())
-{
-    app.UseSwagger();
-    app.UseSwaggerUI();
-}
+app.UseSwagger();
+app.UseSwaggerUI();
 
 app.UseHttpsRedirection();
-
 app.UseAuthorization();
 
 app.MapControllers();
