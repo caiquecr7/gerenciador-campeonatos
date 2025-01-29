de5393928c66a13bd2738ef4845cce7952835a00
Caique Castro Rodrigues <fh203@t-systems.com.br>
2025-01-28 23:21:37 -0300

    [FIX] - Ajuste nas querys manuais do arquivo presente no projeto

--- scripts-sql/script.sql
+++ scripts-sql/script.sql
@@ -3,30 +3,30 @@ CREATE TABLE Team (
     Name NVARCHAR(100) NOT NULL,
     City NVARCHAR(50) NOT NULL,
     FoundationYear INT NOT NULL,
-    CreatedAt DATETIME NOT NULL,
-    UpdatedAt DATETIME
+    Created_At DATETIME NOT NULL,
+    Updated_At DATETIME
 );
 
 CREATE TABLE Player (
     Id INT IDENTITY(1,1) PRIMARY KEY,
     Name NVARCHAR(100) NOT NULL,
-    Position INT NOT NULL, -- PlayerPosition (presume-se que é um enum)
+    Position CHAR(2) NOT NULL,
     Age INT NOT NULL,
     TeamId INT NOT NULL,
-    CreatedAt DATETIME NOT NULL,
-    UpdatedAt DATETIME,
+    Created_At DATETIME NOT NULL,
+    Updated_At DATETIME,
     CONSTRAINT FK_Player_Team FOREIGN KEY (TeamId) REFERENCES Team(Id)
 );
 
 CREATE TABLE Match (
     Id INT IDENTITY(1,1) PRIMARY KEY,
-    GoalsHomeTeam INT NOT NULL,
-    GoalsAwayTeam INT NOT NULL,
+    Goals_Home_Team INT NOT NULL,
+    Goals_Away_Team INT NOT NULL,
     Date DATETIME NOT NULL,
     HomeTeamId INT NOT NULL,
     AwayTeamId INT NOT NULL,
-    CreatedAt DATETIME NOT NULL,
-    UpdatedAt DATETIME,
+    Created_At DATETIME NOT NULL,
+    Updated_At DATETIME,
     CONSTRAINT FK_Match_HomeTeam FOREIGN KEY (HomeTeamId) REFERENCES Team(Id),
     CONSTRAINT FK_Match_AwayTeam FOREIGN KEY (AwayTeamId) REFERENCES Team(Id)
 );
